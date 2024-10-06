c3de21ec7c901d32e018ce1c40eb92d73d107c9e
Augustine Aroke <arokeaugustine@gmail.com>
2024-10-06 15:43:54 +0100

    update to products functionality

--- Controllers/InvestController.cs
+++ Controllers/InvestController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KahaTiev.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KahaTiev.Controllers
 {
     public class InvestController : Controller
     {
-       public async Task<IActionResult> Product()
+        private readonly IInvestService _investService;
+        public InvestController(IInvestService investService)
         {
+            _investService = investService;
+        }
+        public async Task<IActionResult> Product()
+        {
+            var products = await _investService.Products();
             return View();
         }
     }
